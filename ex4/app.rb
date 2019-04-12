aefacf093afde061419e393544183b5fa88a7184
Bartlomiej Danek <b.danek@selleo.com>
2019-04-12 16:27:59 +0200

    stage_2

--- ex4/app.rb
+++ ex4/app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
+require 'pg'
 
 set :bind, '0.0.0.0'
 
+con = PG.connect \
+  dbname: ENV.fetch('POSTGRES_DATABASE'),
+	user: ENV.fetch('POSTGRES_USER'),
+	password: ENV.fetch('POSTGRES_PASSWORD'),
+	host: ENV.fetch('POSTGRES_HOST'),
+	port: ENV.fetch('POSTGRES_PORT')
+
 get '/' do
-  "Hello World #{params[:name]}".strip
+  rs_1 = con.exec 'SELECT VERSION()'
+  rs_2 = con.exec 'SELECT NOW()'
+  "Hello from #{ENV.fetch('HOSTNAME')} | #{con.server_version} | #{rs_1.getvalue(0, 0)} | #{rs_2.getvalue(0, 0)}".strip
 end
